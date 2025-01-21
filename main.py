611ff78ab6670a1eeb4f95fd2a302d9841f9e988
korneevDev <korneev.dev@gmail.com>
2025-01-21 12:09:24 +0300

    Getting total price for one partner

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ con.close()
 
 
 """
-Алгоритм расчета скидок:
+Алгоритм расчета скидок для одного партнёра:
 
 + 1. Получить все продажи по конкретному партнёру
 + 2. Получить цены товаров, которые были проданы 
@@ -36,7 +36,7 @@ con.close()
 
 """
 
-def calculate_discount(partners):
+def calculate_discount(partner):
     ### 1-й блок: получение итогового размера стоимости продаж
 
     connection, cursor = get_connection()
@@ -44,29 +44,28 @@ def calculate_discount(partners):
     cursor.execute("SELECT partner, SUM(product_count * products.min_cost) "
                    "FROM partner_products JOIN products "
                    "ON products.product_id = partner_products.product "
-                   "GROUP BY partner;")
+                   "WHERE partner = %(id)s "
+                   "GROUP BY partner;", {'id': partner.partner_id})
     sale_data_from_db = cursor.fetchall()
     sale_data = [Sale(x[0], x[1]) for x in sale_data_from_db]
 
     cursor.close()
     connection.close()
+    total_price = 0
+    for sale in sale_data:
+        if sale.partner == partner.partner_id:
+            total_price = sale.total_price
+            break
 
-    for partner in partners:
-        total_price = 0
-        for sale in sale_data:
-            if sale.partner == partner.partner_id:
-                total_price = sale.total_price
-                break
-
-        ### 2-й блок: получение скидки
-        if total_price < 10_000:
-            partner.partner_discount = 0
-        elif 10_000 <= total_price < 50_000:
-            partner.partner_discount = 5
-        elif 50000 <= total_price < 300000:
-            partner.partner_discount = 10
-        else:
-            partner.partner_discount = 15
+    ### 2-й блок: получение скидки
+    if total_price < 10_000:
+        partner.partner_discount = 0
+    elif 10_000 <= total_price < 50_000:
+        partner.partner_discount = 5
+    elif 50000 <= total_price < 300000:
+        partner.partner_discount = 10
+    else:
+        partner.partner_discount = 15
 
 
 '''
